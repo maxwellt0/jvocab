ee89e327c8740cb76479e50ffce241f65ac0456e
Max <maksimvinnik@gmail.com>
2017-09-24 23:29:47 +0300

    Configured the base. Added new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {DictionaryComponent} from './dictionary/dictionary.component';
+import {AngularFontAwesomeModule} from 'angular-font-awesome/angular-font-awesome';
+import {RouterModule, Routes} from "@angular/router";
+import {AppComponent} from "./app.component";
 
-import { AppComponent } from './app.component';
+const appRoutes: Routes = [
+  {path: 'dictionary', component: DictionaryComponent},
+];
 
 @NgModule({
   declarations: [
+    DictionaryComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
